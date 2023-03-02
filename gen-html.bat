b62da4088efc2e06136446ecc227c0c6aa34bd86
Mary Jungers <mjungers@ihe.net>
2023-03-02 14:51:09 -0600

    add the gen-html file

---
+++ gen-html.bat
@@ -0,0 +1,1 @@
+pandoc -r gfm --template=https://github.com/IHE/publications/wiki/files/ihe_template.html --metadata title="Topologies" --metadata path-prefix="../../" -w html -o index.html topologies.md
